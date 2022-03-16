6f4b7efd9ae7c1d0ed7bed88e7c5c8e6f806b583
Ian Cheifetz <sylvan.iacheifetz09@gmail.com>
2022-03-16 14:55:15 -0700

    added functionality to eventListener for addfriendbutton

--- app.js
+++ app.js
@@ -42,10 +42,17 @@ addMushroomButton.addEventListener('click', () => {
 
 addFriendButton.addEventListener('click', () => {
     // get the name from the input
+    const name = friendInputEl.value;
     // create a new friend object
+    const newFriend = {
+        name: name || `Friend #${Math.floor(Math.random() * 100)}`, satisfaction: 1
+    };
     // push it into the friends state array, passed in as an argument
+    friendData.push(newFriend);
     // reset the input
+    friendInputEl.value = '';
     // display all the friends (use a function here)
+    displayFriends();
 });
 
 function displayFriends() {
@@ -54,7 +61,7 @@ function displayFriends() {
     // for each friend in state . . .
     for (let friend of friendData) {
         // use renderFriend to make a friendEl
-
+        renderFriend(friend);
         // this is a clickable list, so . . .
         //     add an event listener to each friend
         //         and if the friend's satisfaction level is below 3 and you have mushrooms left
